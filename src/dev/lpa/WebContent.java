3a6133317e7dc12a436b6d57eea86a821a0ef077
Michael Kopp <MichaelJG.Kopp@gmail.com>
2024-11-08 22:52:50 +0100

    url 2-step connection and header fields

--- src/dev/lpa/WebContent.java
+++ src/dev/lpa/WebContent.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.net.URLConnection;
 
 public class WebContent {
   
@@ -12,7 +13,16 @@ public class WebContent {
     
     try {
       URL url = new URL("http://jsonplaceholder.typicode.com/todos?id=5");
-      printContents(url.openStream());
+//      printContents(url.openStream());  // NOTE: printing stream content
+      // get info to configure before actual connection, 2-step process
+      URLConnection urlConnection = url.openConnection();
+      System.out.println(urlConnection.getContentType());
+      urlConnection.getHeaderFields()
+        .entrySet()
+        .forEach(System.out::println);
+      System.out.println(urlConnection.getHeaderField("Cache-Control"));
+      urlConnection.connect();  // actual network connection
+      printContents(urlConnection.getInputStream());
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
