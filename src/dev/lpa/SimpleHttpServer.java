3687639a159385db6e3f8e170cba04f192158d40
Michael Kopp <MichaelJG.Kopp@gmail.com>
2024-11-09 14:15:23 +0100

    add input fields

--- src/dev/lpa/SimpleHttpServer.java
+++ src/dev/lpa/SimpleHttpServer.java
@@ -32,17 +32,28 @@ public class SimpleHttpServer {
         if (requestMethod.equals("POST")) {
           visitorCounter++;
         }
+        
+        String firstName = parameters.get("first");
+        String lastName = parameters.get("last");
         String response = """
           <html>
             <body>
               <H1>Hello World from My Http Server</H1>
               <p>Number of Visitors who signed up = %d<p>
               <form method="post">
+                <label for="first">First name:</label>
+                <input type="text" id="first" name="first" value="%s">
+                <br>
+                <label for="last">Last name:</label>
+                <input type="text" id="last" name="last" value="%s">
+                <br>
                 <input type="submit" value="Submit">
               </form>
             </body>
           </html>
-          """.formatted(visitorCounter);
+          """.formatted(visitorCounter,
+          firstName == null ? "" : firstName,
+          lastName == null ? "" : lastName);
         
         byte[] bytes = response.getBytes();
         exchange.sendResponseHeaders(HTTP_OK, bytes.length);
